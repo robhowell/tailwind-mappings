98d311f8c3b341726641b0f07fb3e728714371c9
Rob Howell <3998397+robhowell@users.noreply.github.com>
2022-08-11 17:53:43 +0100

    Add replaceAll function to support replacing all text in older versions of Node

---
+++ src/replaceAll.js
@@ -0,0 +1,9 @@
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+module.exports = replaceAll;
