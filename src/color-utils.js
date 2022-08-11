af63797515b305a0c3adc8f75866908d40a4afce
Rob Howell <3998397+robhowell@users.noreply.github.com>
2022-08-11 21:03:15 +0100

    Add custom color palette to color-utils

--- src/color-utils.js
+++ src/color-utils.js
@@ -1,7 +1,50 @@
-'use strick';
+'use strict';
 
 const chroma = require('chroma-js');
-const colors = require('tailwindcss/colors');
+// const colors = require('tailwindcss/colors');
+const getArbitraryClass = require('./getArbitraryClass');
+
+const colors = {
+  brand: {
+    darker: '#0a545f',
+    dark: '#016a78',
+    DEFAULT: '#107c8c',
+    light: '#cff0fc',
+    lighter: '#eaf3f4',
+    lightest: '#f2f8f9',
+  },
+  white: '#ffffff',
+  whiteOverlay: 'rgba(255, 255, 255, 0.6)',
+  black: '#000000',
+  blackOverlay: 'rgba(0, 0, 0, 0.6)',
+  gray: {
+    dark: '#262626',
+    DEFAULT: '#424242',
+    light1: '#5a5a5a',
+    light2: '#737373',
+    light3: '#919191',
+    light4: '#cccccc',
+    light5: '#e5e5e5',
+    light6: '#f1f1f1',
+    light7: '#f6f6f6',
+    light8: '#fafafa',
+  },
+  grayOverlay: 'rgba(38, 38, 38, 0.4)',
+  red: {
+    dark: '#832829',
+    DEFAULT: '#b85455',
+    light: '#f7f1f2',
+  },
+  yellow: {
+    dark: '#ab8d40',
+    DEFAULT: '#e4c272',
+    light: '#f7f2e8',
+  },
+  facebook: '#1977f2',
+  transparent: 'transparent',
+  unset: 'unset',
+  initial: 'initial',
+};
 
 const TAILWIND_CLASSES = require('./constants');
 // https://gist.github.com/ryancat/9972419b2a78f329ce3aebb7f1a09152
@@ -22,38 +65,62 @@ function deltaRgb(rgb1, rgb2) {
   return Math.sqrt(2 * drp2 + 4 * dgp2 + 3 * dbp2 + (t * (drp2 - dbp2)) / 256);
 }
 
-const defaultColors = [
-  'pink',
-  'purple',
-  'indigo',
-  'blue',
-  'green',
-  'yellow',
-  'red',
-  'coolGray',
-];
+const stripDefault = (string) => {
+  return string.replace('-DEFAULT', '');
+};
 
 function getProximateColor(decl) {
   const twColors = Object.keys(colors)
-    .filter((c) => defaultColors.includes(c))
     .map((c) => {
       const shades = colors[c];
-      const palette = Object.keys(shades).map((s) => {
-        const _c = c.replace('coolG', 'g');
-        return {
-          'background-color': `bg-${_c}-${s}`,
-          background: `bg-${_c}-${s}`,
-          color: `text-${_c}-${s}`,
-          border: `border-${_c}-${s}`,
-          'border-color': `border-${_c}-${s}`,
-          hex: shades[s],
-        };
-      });
-      return palette;
+
+      if (typeof shades === 'object') {
+        const palette = Object.keys(shades).map((s) => {
+          return {
+            'background-color': stripDefault(`bg-${c}-${s}`),
+            background: stripDefault(`bg-${c}-${s}`),
+            color: stripDefault(`text-${c}-${s}`),
+            border: stripDefault(`border-${c}-${s}`),
+            'border-color': stripDefault(`border-${c}-${s}`),
+            hex: shades[s],
+          };
+        });
+
+        return palette;
+      }
+
+      const s = shades;
+
+      return {
+        'background-color': stripDefault(`bg-${c}-${s}`),
+        background: stripDefault(`bg-${c}-${s}`),
+        color: stripDefault(`text-${c}-${s}`),
+        border: stripDefault(`border-${c}-${s}`),
+        'border-color': stripDefault(`border-${c}-${s}`),
+        hex: s,
+      };
     })
     .flat();
 
+  if (decl.value === 'transparent') {
+    return twColors.find((item) => item.hex === 'transparent')[decl.prop];
+  }
+
+  if (decl.value === 'unset') {
+    return twColors.find((item) => item.hex === 'unset')[decl.prop];
+  }
+
+  if (decl.value === 'initial') {
+    return twColors.find((item) => item.hex === 'initial')[decl.prop];
+  }
+
   const sorted = twColors
+    .filter(
+      (item) =>
+        item.hex !== 'transparent' &&
+        item.hex !== 'unset' &&
+        item.hex !== 'initial'
+    )
     .map((c) => {
       let _val = decl.value;
       if (decl.prop === 'border') {
@@ -75,10 +142,9 @@ function getProximateColor(decl) {
 
 function getColorUtils(decl) {
   // TODO: Add support for linear-gradients, or throw an error
-  if (decl.value.includes('linear-gradient')) return ' ';
-
-  // TODO: Add support for URLs, or throw an error
-  if (decl.value.includes('url')) return ' ';
+  if (decl.value.includes('linear-gradient') || decl.value.includes('url')) {
+    return getArbitraryClass(decl);
+  }
 
   const hash = TAILWIND_CLASSES[decl.prop];
 
