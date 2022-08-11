be671fa8136b83af4ae3c19c452f665b43e0139a
Rob Howell <3998397+robhowell@users.noreply.github.com>
2022-08-11 17:55:52 +0100

    Add support to return arbitrary CSS for unsupported properties

--- src/tailwind-utils.js
+++ src/tailwind-utils.js
@@ -5,9 +5,9 @@ const getSpacingUtils = require('./spacing-utils');
 const debug = require('debug')('tailwind-mappings');
 
 const { getBorderUtils, getBorderColorUtils } = require('./border-utils');
-
 const getBorderRadiusUtils = require('./border-radius-utils');
 const getColorUtils = require('./color-utils');
+const replaceAll = require('./replaceAll');
 
 const getValueBetweenBrackets = (value) => {
   const openBracket = value.indexOf('(');
@@ -15,7 +15,7 @@ const getValueBetweenBrackets = (value) => {
   return value.substring(openBracket + 1, closeBracket);
 };
 
-const getArbitraryClass = (prop, value) => {
+const getUnknownClass = (prop, value) => {
   if (prop === 'transform') {
     if (value.includes('scaleX'))
       return `scale-x-[${getValueBetweenBrackets(value)}]`;
@@ -47,60 +47,71 @@ function getTailwindUtils(incomingDecl) {
     value: val,
   };
 
-  let output = '';
-
-  switch (decl.prop) {
-    case 'margin':
-    case 'margin-left':
-    case 'margin-right':
-    case 'margin-top':
-    case 'margin-bottom':
-    case 'padding':
-    case 'padding-left':
-    case 'padding-right':
-    case 'padding-top':
-    case 'padding-bottom':
-      output = getSpacingUtils(decl, decl.prop);
-      break;
-
-    case 'border':
-      output = getBorderUtils(decl);
-      break;
-
-    case 'color':
-    case 'background-color':
-    case 'background':
-      if (decl.value !== 'inherit' && !decl.value.includes('var')) {
-        output = getColorUtils(decl);
-      }
-      break;
-
-    case 'border-radius':
-      output = getBorderRadiusUtils(decl);
-      break;
-
-    case 'border-color':
-      output = getBorderColorUtils(decl);
-      break;
-
-    case 'opacity':
-      if (decl.value.startsWith('.')) {
-        decl.value = '0' + decl.value;
-      }
-      output = prop[decl.value] || '';
-      break;
-
-    default:
-      if (prop && prop[val]) {
-        output = prop[val] || '';
-      } else if (prop) {
-        output = getArbitraryClass(decl.prop, val);
-      } else {
-        console.error('Unknown prop: ', decl.prop);
-      }
+  if (['animation'].includes(decl.prop)) {
+    return `[${decl.prop}:${replaceAll(
+      replaceAll(decl.value, ' ', '_'),
+      '\n',
+      '_'
+    )}]`;
   }
 
-  return output;
+  if (
+    [
+      'margin',
+      'margin-left',
+      'margin-right',
+      'margin-top',
+      'margin-bottom',
+      'padding',
+      'padding-left',
+      'padding-right',
+      'padding-top',
+      'padding-bottom',
+    ].includes(decl.prop)
+  ) {
+    return getSpacingUtils(decl, decl.prop);
+  }
+
+  if (decl.prop === 'border') {
+    return getBorderUtils(decl);
+  }
+
+  if (['color', 'background-color', 'background'].includes(decl.prop)) {
+    if (decl.value !== 'inherit' && !decl.value.includes('var')) {
+      return getColorUtils(decl);
+    }
+
+    console.error('Unknown color', decl.value);
+
+    return '';
+  }
+
+  if (decl.prop === 'border-radius') {
+    return getBorderRadiusUtils(decl);
+  }
+
+  if (decl.prop === 'border-color') {
+    return getBorderColorUtils(decl);
+  }
+
+  if (decl.prop === 'opacity') {
+    if (decl.value.startsWith('.')) {
+      decl.value = '0' + decl.value;
+    }
+
+    // TODO: Add support for unknown opacity values
+    return prop[decl.value] || '';
+  }
+
+  if (prop && prop[val]) {
+    return prop[val] || '';
+  } else if (prop) {
+    return getUnknownClass(decl.prop, val);
+  }
+
+  console.error('Unknown prop: ', decl.prop);
+
+  return '';
 }
 
 module.exports = getTailwindUtils;
