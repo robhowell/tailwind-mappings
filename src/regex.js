5bdee77f920dc1f22cedb9dc6bf389df043428bd
Rob Howell <3998397+robhowell@users.noreply.github.com>
2023-05-18 13:06:57 +0100

    Improve subSelectorRegex to handle all-item selector (*)

--- src/regex.js
+++ src/regex.js
@@ -3,7 +3,7 @@ const firstSubSelectorRegex =
   /^(?:[.#]?[a-zA-Z0-9_-]+)(?::{1,2}[a-zA-Z0-9_-]*(?:\([:.a-zA-Z0-9_-]*\))*)*/g;
 const pseudoAttributeRegex = /::?[a-zA-Z0-9_-]*(?:\([a-zA-Z0-9:.+ *_-]*\))?/g;
 const subSelectorRegex =
-  /(?:[.#]?[a-zA-Z0-9_-]+)(?::{1,2}[a-zA-Z0-9_-]*(?:\([:.a-zA-Z0-9_-]*\))*)*/g;
+  /((?:[.#]?[a-zA-Z0-9_-]+)(?::{1,2}[a-zA-Z0-9_-]*(?:\([:.a-zA-Z0-9_-]*\))*)*|\*)/g;
 
 module.exports = {
   classNameRegex,
